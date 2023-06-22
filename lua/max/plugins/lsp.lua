f7ffdb6a47e80204c868ae30db5d03c6754be288
maxime50 <maxime.romero50@gmail.com>
2023-06-22 05:13:38 -0400

    WIP

--- lua/max/plugins/lsp.lua
+++ lua/max/plugins/lsp.lua
@@ -22,11 +22,15 @@ return {
     -- Snippets
     { 'L3MON4D3/LuaSnip' },
     { 'rafamadriz/friendly-snippets' },
+
+    -- Formatter
+    -- { 'jose-elias-alvarez/null-ls' },
+    -- { 'jay-babu/mason-null-ls' },
+
   },
   config = function()
     local lsp = require("lsp-zero")
-
-    lsp.preset("recommended")
+    lsp.preset({})
 
     -- lsp.ensure_installed({
     --   'tsserver',
@@ -34,25 +38,7 @@ return {
     -- })
 
     -- Fix Undefined global 'vim'
-    lsp.nvim_workspace()
-
-
-    local cmp = require('cmp')
-    local cmp_select = { behavior = cmp.SelectBehavior.Replace } -- or cmp.SelectBehavior.Select
-    local cmp_mappings = lsp.defaults.cmp_mappings({
-      ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-      ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-      ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
-      ["<C-Space>"] = cmp.mapping.complete(),
-    })
-
-    cmp_mappings['<Tab>'] = nil
-    cmp_mappings['<S-Tab>'] = nil
-
-    lsp.setup_nvim_cmp({
-      mapping = cmp_mappings
-    })
+    require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
     lsp.set_preferences({
       suggest_lsp_servers = false,
@@ -66,20 +52,95 @@ return {
 
     lsp.on_attach(function(client, bufnr)
       local opts = { buffer = bufnr, remap = false }
-
-      vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
       vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-      vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-      vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+      vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+      vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+      vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+      vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
+      vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+      vim.keymap.set("n", "gs", function() vim.lsp.buf.signature_help() end, opts)
       vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
       vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-      vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts)
-      vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.references() end, opts)
-      vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
       vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+      vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+      vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
+      vim.keymap.set("n", "<F2>", function() vim.lsp.buf.rename() end, opts)
+      vim.keymap.set({ "n", "x" }, "<F3>", function() vim.lsp.buf.format({ async = true }) end, opts)
     end)
 
+    lsp.format_on_save({
+      format_opts = {
+        async = false,
+        timeout_ms = 10000,
+      },
+      servers = {
+        ['lua_ls'] = {'lua'},
+        ['rust_analyzer'] = {'rust'},
+        -- if you have a working setup with null-ls
+        -- you can specify filetypes it can format.
+        -- ['null-ls'] = {'javascript', 'typescript'},
+      }
+    })
+
     lsp.setup()
+
+    local cmp = require('cmp')
+    local cmp_select = { behavior = cmp.SelectBehavior.Replace } -- or cmp.SelectBehavior.Select
+    local cmp_mappings = lsp.defaults.cmp_mappings({
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+      ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+      ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-d>'] = cmp.mapping.scroll_docs(4),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    })
+
+    -- luasnip
+    require("luasnip.loaders.from_vscode").lazy_load()
+    local cmp_action = require('lsp-zero').cmp_action()
+    cmp_mappings['<Tab>'] = cmp_action.luasnip_jump_forward()
+    cmp_mappings['<S-Tab>'] = cmp_action.luasnip_jump_backward()
+
+    -- cmp_mappings['<Tab>'] = nil
+    -- cmp_mappings['<S-Tab>'] = nil
+
+
+    -- or lsp.setup_nvim_cmp
+    cmp.setup({
+	    sources = {
+		    {name = "path"},
+		    {name = "nvim_lsp"},
+		    {name = "nvim_lua"},
+		    {name = "buffer", keyword_length = 3},
+		    {name = "luasnip", keyword_length = 2},
+	    },
+	    mapping = cmp_mappings
+    })
+
+--    local null_ls = require('null-ls')
+
+-- null_ls.setup({
+--   sources = {
+    -- Here you can add tools not supported by mason.nvim
+    -- make sure the source name is supported by null-ls
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
+--   }
+-- })
+
+-- See mason-null-ls.nvim's documentation for more details:
+-- https://github.com/jay-babu/mason-null-ls.nvim#setup
+-- require('mason-null-ls').setup({
+ --  ensure_installed = nil,
+ --  automatic_installation = false, -- You can still set this to `true`
+ --  handlers = {
+ --      -- Here you can add functions to register sources.
+ --      -- See https://github.com/jay-babu/mason-null-ls.nvim#handlers-usage
+ --      --
+  --     -- If left empty, mason-null-ls will  use a "default handler"
+ --      -- to register all sources
+ --  }
+-- })
 
     vim.diagnostic.config({
       virtual_text = false
---
+++ lua/max/plugins/telescope.lua
@@ -0,0 +1,123 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    {
+      "nvim-telescope/telescope-file-browser.nvim",
+    },
+  },
+  keys = {
+    {
+      "sf",
+      function() require("telescope.builtin").find_files() end,
+      desc = "Search Files",
+    },
+    {
+      "sp",
+      function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+      desc = "Search Plugin Files",
+    },
+    {
+      "so",
+      function()
+        require("telescope").extensions.file_browser.file_browser({
+          path = "%:p:h",
+          cwd = vim.fn.expand("%:p:h"),
+          respect_gitignore = false,
+          hidden = false,
+          grouped = true,
+          initial_mode = "normal",
+          previewer = false,
+          layout_config = { height = 40 },
+        })
+      end,
+    },
+    {
+      "st",
+      function() require("telescope.builtin").live_grep() end,
+      desc = "Search Text",
+    },
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local fb_actions = require("telescope").extensions.file_browser.actions
+
+    telescope.setup({
+      defaults = {
+        prompt_prefix = " ",
+        selection_caret = "❯ ",
+        path_display = { "truncate" },
+        sorting_strategy = "ascending",
+        layout_config = {
+          horizontal = {
+            prompt_position = "top",
+            preview_width = 0.55,
+            results_width = 0.8,
+          },
+          vertical = {
+            mirror = false,
+          },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
+
+        mappings = {
+          i = {
+            ["<C-j>"] = actions.cycle_history_next,
+            ["<C-k>"] = actions.cycle_history_prev,
+          },
+          n = { ["q"] = actions.close },
+        },
+      },
+      extensions = {
+        file_browser = {
+          theme = "dropdown",
+          -- disables netrw and use telescope-file-browser in its place
+          hijack_netrw = false,
+          select_buffer = false, -- auto focus file from current buffer
+          mappings = {
+            -- your custom insert mode mappings
+            ["i"] = {
+              ["<C-w>"] = function() vim.cmd("normal vbd") end,
+            },
+            ["n"] = {
+              -- your custom normal mode mappings
+              ["n"] = fb_actions.create,
+              ["h"] = fb_actions.goto_parent_dir,
+              ["l"] = actions.select_default,
+              ["gh"] = fb_actions.goto_home_dir,
+              ["H"] = fb_actions.toggle_hidden,
+              ["v"] = fb_actions.toggle_all,
+              ["p"] = fb_actions.copy,
+              ["m"] = fb_actions.move,
+              ["q"] = actions.close,
+              ["/"] = function() vim.cmd("startinsert") end,
+            },
+          },
+        },
+      },
+    })
+    telescope.load_extension("fzf")
+    telescope.load_extension("file_browser")
+
+    -- vim.keymap.set("n", "st", function() require("telescope.builtin").live_grep() end)
+    -- vim.keymap.set("n", "<leader>sgs", function() require("telescope.builtin").git_status() end)
+    -- vim.keymap.set("n", "<leader>sgb", function() require("telescope.builtin").git_branches() end)
+    -- vim.keymap.set("n", "<leader>sgc", function() require("telescope.builtin").git_commits() end)
+    -- vim.keymap.set("n", "sf", function() require("telescope.builtin").find_files() end)
+    -- vim.keymap.set("n", "<leader>sb", function() require("telescope.builtin").buffers() end)
+    -- vim.keymap.set("n", "<leader>sh", function() require("telescope.builtin").help_tags() end)
+    -- vim.keymap.set("n", "<leader>sm", function() require("telescope.builtin").marks() end)
+    -- vim.keymap.set("n", "<leader>sh", function() require("telescope.builtin").help_tags() end)
+    -- vim.keymap.set("n", "<leader>sm", function() require("telescope.builtin").man_pages() end)
+    -- vim.keymap.set("n", "<leader>sn", function() require("telescope").extensions.notify.notify() end)
+    -- vim.keymap.set("n", "<leader>sr", function() require("telescope.builtin").registers() end)
+    -- vim.keymap.set("n", "<leader>sk", function() require("telescope.builtin").keymaps() end)
+    -- vim.keymap.set("n", "<leader>sc", function() require("telescope.builtin").commands() end)
+    -- vim.keymap.set("n", "<leader>ss", function() require("telescope.builtin").resume() end)
+    -- vim.keymap.set("n", "<leader>sl", function() require("telescope.builtin").resume() end)
+  end,
+}
