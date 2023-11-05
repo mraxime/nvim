31fdf14601fb12f4d74d0d03230a4f89655fda4f
mraxime <maxime.romero50@gmail.com>
2023-11-05 01:42:46 -0400

    WIP

--- lua/max/plugins/lualine.lua
+++ lua/max/plugins/lualine.lua
@@ -1,11 +1,46 @@
 -- Fancier statusline
 return {
-	"tamighi/lualine.nvim", -- fork with harpoon_files plugin
-	-- "nvim-lualine/lualine.nvim",
-	enabled = false,
-	branch = "feat/harpoon_files",
+	-- "tamighi/lualine.nvim", -- fork with harpoon_files plugin
+	"nvim-lualine/lualine.nvim",
+	enabled = true,
 	event = "VeryLazy",
 	config = function()
+		local hp_marks = require("harpoon.mark")
+		local devicons = require("nvim-web-devicons")
+
+		function Harpoon_files()
+			local contents = {}
+
+			for idx = 1, hp_marks.get_length() do
+				local file_path = hp_marks.get_marked_file_name(idx)
+				local file_name
+				if file_path == "" then
+					file_name = "(empty)"
+				else
+					file_name = vim.fn.fnamemodify(file_path, ":t")
+				end
+				local dev, _ = devicons.get_icon(file_name)
+
+				local current_file_path = vim.fn.expand("%:f")
+
+				local prev = ""
+				if idx ~= 1 then
+					prev = " "
+				end
+				local next = ""
+				if idx < hp_marks.get_length() then
+					next = " "
+				end
+				if file_path == current_file_path then
+					contents[idx] = string.format("%%#lualine_b_normal#%s%s %s%s", prev, dev, file_name, next)
+				else
+					contents[idx] = string.format("%%#lualine_b_inactive#%s%s %s%s", prev, dev, file_name, next)
+				end
+			end
+
+			return table.concat(contents)
+		end
+
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
@@ -15,13 +50,7 @@ return {
 			},
 			sections = {},
 			inactive_sections = {},
-			tabline = {
-				lualine_a = {
-					{
-						"harpoon_files",
-					},
-				},
-			},
+			tabline = { lualine_a = { { Harpoon_files } } },
 		})
 	end,
 }
