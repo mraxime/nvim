f7a603c258b57aef7b6e9a81245620bf01bd9cfc
mraxime <mraxime@gmail.com>
2023-12-02 11:39:14 -0500

    fzf: remove file-browser

--- lua/max/plugins/telescope.lua
+++ lua/max/plugins/telescope.lua
@@ -4,7 +4,6 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
-		"nvim-telescope/telescope-file-browser.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
 	keys = {
@@ -43,13 +42,6 @@ return {
 			end,
 			desc = "Resume Telescope",
 		},
-		{
-			"so",
-			function()
-				require("telescope").extensions.file_browser.file_browser()
-			end,
-			desc = "Search File Browser",
-		},
 		-- old muscle memory
 		{
 			"<leader>f",
@@ -69,7 +61,6 @@ return {
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
-		local fb_actions = require("telescope").extensions.file_browser.actions
 
 		telescope.setup({
 			defaults = {
@@ -117,53 +108,7 @@ return {
 					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
 				},
 			},
-			extensions = {
-				file_browser = {
-					theme = "dropdown",
-					hijack_netrw = false,
-					select_buffer = true, -- auto focus file from current buffer
-					initial_browser = "tree",
-					auto_depth = true,
-					depth = 1,
-					-- Default options
-					path = "%:p:h",
-					cwd = vim.fn.expand("%:p:h"),
-					respect_gitignore = false,
-					hidden = false,
-					grouped = true,
-					initial_mode = "normal",
-					previewer = false,
-					layout_config = { height = 0.8, width = 52 },
-					display_stat = false,
-					-- Mappings
-					mappings = {
-						-- your custom insert mode mappings
-						["i"] = {
-							["<C-w>"] = function()
-								vim.cmd("normal vbd")
-							end,
-						},
-						["n"] = {
-							-- your custom normal mode mappings
-							["a"] = fb_actions.create,
-							["h"] = fb_actions.goto_parent_dir,
-							["l"] = actions.select_default,
-							["gh"] = fb_actions.goto_home_dir,
-							["H"] = fb_actions.toggle_hidden,
-							["v"] = fb_actions.toggle_all,
-							["p"] = fb_actions.copy,
-							["m"] = fb_actions.move,
-							["x"] = fb_actions.move,
-							["q"] = actions.close,
-							["/"] = function()
-								vim.cmd("startinsert")
-							end,
-						},
-					},
-				},
-			},
 		})
 		telescope.load_extension("fzf")
-		telescope.load_extension("file_browser")
 	end,
 }
