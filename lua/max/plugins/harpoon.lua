2afb97b49d88d45d806eb47cd04a19c691f9ffe2
mraxime <mraxime@gmail.com>
2024-04-26 12:07:55 -0400

    WIP

--- lua/max/plugins/harpoon.lua
+++ lua/max/plugins/harpoon.lua
@@ -1,121 +1,145 @@
 return {
-	"ThePrimeagen/harpoon",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	lazy = false,
-	enabled = true,
-	version = false,
-	branch = "harpoon2",
-	config = function()
-		local harpoon = require("harpoon")
-		harpoon:setup({ settings = {
-			save_on_toggle = true,
-			sync_on_ui_close = true,
-		} })
-	end,
-	keys = {
-		-- Menu Toggle
-		{
-			"gh",
-			function()
-				local harpoon = require("harpoon")
-				harpoon.ui:toggle_quick_menu(harpoon:list())
-			end,
-			desc = "Toggle harpoon menu",
-		},
-		{
-			"sm",
-			function()
-				local harpoon = require("harpoon")
-				harpoon.ui:toggle_quick_menu(harpoon:list())
-			end,
-			desc = "Toggle harpoon menu",
-		},
-		{
-			"ms",
-			function()
-				local harpoon = require("harpoon")
-				harpoon.ui:toggle_quick_menu(harpoon:list())
-			end,
-			desc = "Toggle harpoon menu",
-		},
-		{
-			"sh",
-			function()
-				local harpoon = require("harpoon")
-				harpoon.ui:toggle_quick_menu(harpoon:list())
-			end,
-			desc = "Toggle harpoon menu",
-		},
+	{
+		"ThePrimeagen/harpoon",
+		enabled = true,
+		dependencies = { "nvim-lua/plenary.nvim" },
+		lazy = false,
+		version = false,
+		branch = "harpoon2",
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup({ settings = {
+				save_on_toggle = true,
+				sync_on_ui_close = true,
+			} })
+		end,
+		keys = {
+			-- Menu Toggle
+			{
+				"gh",
+				function()
+					local harpoon = require("harpoon")
+					harpoon.ui:toggle_quick_menu(harpoon:list())
+				end,
+				desc = "Toggle harpoon menu",
+			},
+			{
+				"sm",
+				function()
+					local harpoon = require("harpoon")
+					harpoon.ui:toggle_quick_menu(harpoon:list())
+				end,
+				desc = "Toggle harpoon menu",
+			},
+			{
+				"ms",
+				function()
+					local harpoon = require("harpoon")
+					harpoon.ui:toggle_quick_menu(harpoon:list())
+				end,
+				desc = "Toggle harpoon menu",
+			},
+			{
+				"sh",
+				function()
+					local harpoon = require("harpoon")
+					harpoon.ui:toggle_quick_menu(harpoon:list())
+				end,
+				desc = "Toggle harpoon menu",
+			},
 
-		-- Menu Add
-		{
-			"ga",
-			function()
-				local harpoon = require("harpoon")
-				harpoon:list():append()
-				-- harpoon.ui:toggle_quick_menu(harpoon:list())
-			end,
-			desc = "Add buffer to harpoon",
-		},
-		{
-			"mm",
-			function()
-				local harpoon = require("harpoon")
-				harpoon:list():append()
-			end,
-			desc = "Add buffer to harpoon",
-		},
-		{
-			"ma",
-			function()
-				local harpoon = require("harpoon")
-				harpoon:list():append()
-				harpoon:list():open_quick_menu(harpoon:list())
-			end,
-			desc = "Add buffer to harpoon",
-		},
+			-- Menu Add
+			{
+				"ga",
+				function()
+					local harpoon = require("harpoon")
+					harpoon:list():add()
+					-- harpoon.ui:toggle_quick_menu(harpoon:list())
+				end,
+				desc = "Add buffer to harpoon",
+			},
+			{
+				"mm",
+				function()
+					local harpoon = require("harpoon")
+					harpoon:list():add()
+				end,
+				desc = "Add buffer to harpoon",
+			},
+			{
+				"ma",
+				function()
+					local harpoon = require("harpoon")
+					harpoon:list():add()
+					harpoon:list():open_quick_menu(harpoon:list())
+				end,
+				desc = "Add buffer to harpoon",
+			},
 
-		-- Navigation
-		{
-			"gu",
-			function()
-				local harpoon = require("harpoon")
-				harpoon:list():select(1)
-			end,
-			desc = "Set current harpoon to file 1",
-		},
-		{
-			"gi",
-			function()
-				local harpoon = require("harpoon")
-				harpoon:list():select(2)
-			end,
-			desc = "Set current harpoon to file 1",
-		},
-		{
-			"go",
-			function()
-				local harpoon = require("harpoon")
-				harpoon:list():select(3)
-			end,
-			desc = "Set current harpoon to file 1",
-		},
-		{
-			"gp",
-			function()
-				local harpoon = require("harpoon")
-				harpoon:list():select(4)
-			end,
-			desc = "Set current harpoon to file 1",
-		},
+			-- Navigation
+			{
+				"gu",
+				function()
+					local harpoon = require("harpoon")
+					harpoon:list():select(1)
+				end,
+				desc = "Set current harpoon to file 1",
+			},
+			{
+				"gi",
+				function()
+					local harpoon = require("harpoon")
+					harpoon:list():select(2)
+				end,
+				desc = "Set current harpoon to file 1",
+			},
+			{
+				"go",
+				function()
+					local harpoon = require("harpoon")
+					harpoon:list():select(3)
+				end,
+				desc = "Set current harpoon to file 1",
+			},
+			{
+				"gp",
+				function()
+					local harpoon = require("harpoon")
+					harpoon:list():select(4)
+				end,
+				desc = "Set current harpoon to file 1",
+			},
 
-		-- Reset
-		{
-			"mc",
-			function()
-				require("harpoon.mark").clear_all()
-			end,
-			desc = "Reset harpoon",
+			-- Reset
+			{
+				"mc",
+				function()
+					require("harpoon.mark").clear_all()
+				end,
+				desc = "Reset harpoon",
+			},
+		},
+	},
+
+	{
+		"cbochs/grapple.nvim",
+		enabled = false,
+		opts = {
+			scope = "git", -- also try out "git_branch"
+			icons = false, -- setting to "true" requires "nvim-web-devicons"
+			status = false,
+		},
+		keys = {
+			{ "<leader>a", "<cmd>Grapple toggle<cr>", desc = "Tag a file" },
+			{ "<c-e>", "<cmd>Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
+
+			{ "<c-h>", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
+			{ "<c-t>", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
+			{ "<c-n>", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
+			{ "<c-s>", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
+
+			{ "<c-s-n>", "<cmd>Grapple cycle_tags next<cr>", desc = "Go to next tag" },
+			{ "<c-s-p>", "<cmd>Grapple cycle_tags prev<cr>", desc = "Go to previous tag" },
 		},
 	},
 }
--- lua/max/plugins/lualine.lua
+++ lua/max/plugins/lualine.lua
@@ -3,43 +3,54 @@ return {
 	-- "tamighi/lualine.nvim", -- fork with harpoon_files plugin
 	"nvim-lualine/lualine.nvim",
 	enabled = false,
+	dependencies = {
+		{
+			"letieu/harpoon-lualine",
+			dependencies = {
+				{
+					"ThePrimeagen/harpoon",
+					branch = "harpoon2",
+				},
+			},
+		},
+	},
 	event = "VeryLazy",
 	config = function()
-		local hp_marks = require("harpoon.mark")
-		local devicons = require("nvim-web-devicons")
-
-		function Harpoon_files()
-			local contents = {}
-
-			for idx = 1, hp_marks.get_length() do
-				local file_path = hp_marks.get_marked_file_name(idx)
-				local file_name
-				if file_path == "" then
-					file_name = "(empty)"
-				else
-					file_name = vim.fn.fnamemodify(file_path, ":t")
-				end
-				local dev, _ = devicons.get_icon(file_name)
-
-				local current_file_path = vim.fn.expand("%:f")
-
-				local prev = ""
-				if idx ~= 1 then
-					prev = " "
-				end
-				local next = ""
-				if idx < hp_marks.get_length() then
-					next = " "
-				end
-				if file_path == current_file_path then
-					contents[idx] = string.format("%%#lualine_b_normal#%s%s %s%s", prev, dev, file_name, next)
-				else
-					contents[idx] = string.format("%%#lualine_b_inactive#%s%s %s%s", prev, dev, file_name, next)
-				end
-			end
-
-			return table.concat(contents)
-		end
+		-- local hp_marks = require("harpoon.mark")
+		-- local devicons = require("nvim-web-devicons")
+		--
+		-- function Harpoon_files()
+		-- 	local contents = {}
+		--
+		-- 	for idx = 1, hp_marks.get_length() do
+		-- 		local file_path = hp_marks.get_marked_file_name(idx)
+		-- 		local file_name
+		-- 		if file_path == "" then
+		-- 			file_name = "(empty)"
+		-- 		else
+		-- 			file_name = vim.fn.fnamemodify(file_path, ":t")
+		-- 		end
+		-- 		local dev, _ = devicons.get_icon(file_name)
+		--
+		-- 		local current_file_path = vim.fn.expand("%:f")
+		--
+		-- 		local prev = ""
+		-- 		if idx ~= 1 then
+		-- 			prev = " "
+		-- 		end
+		-- 		local next = ""
+		-- 		if idx < hp_marks.get_length() then
+		-- 			next = " "
+		-- 		end
+		-- 		if file_path == current_file_path then
+		-- 			contents[idx] = string.format("%%#lualine_b_normal#%s%s %s%s", prev, dev, file_name, next)
+		-- 		else
+		-- 			contents[idx] = string.format("%%#lualine_b_inactive#%s%s %s%s", prev, dev, file_name, next)
+		-- 		end
+		-- 	end
+		--
+		-- 	return table.concat(contents)
+		-- end
 
 		require("lualine").setup({
 			options = {
@@ -48,11 +59,13 @@ return {
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
 			},
-			sections = {},
-			inactive_sections = {},
-			tabline = {
-				lualine_a = { Harpoon_files },
+			sections = {
+				lualine_c = { "harpoon2" },
 			},
+			inactive_sections = {},
+			-- tabline = {
+			-- 	lualine_a = { Harpoon_files },
+			-- },
 		})
 	end,
 }
