f0933d96944b13beb27098d7f7972009a6743f90
mraxime <mraxime@gmail.com>
2025-03-08 00:37:45 -0500

    WIP

--- lua/max/plugins/cmp.lua
+++ lua/max/plugins/cmp.lua
@@ -10,10 +10,19 @@ return {
 		},
 		sources = {
 			default = {
+				"lazydev",
 				"lsp",
 				"path",
 				-- "snippets",
 				-- "buffer",
+			},
+			providers = {
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					-- make lazydev completions top priority (see `:h blink.cmp`)
+					score_offset = 100,
+				},
 			},
 		},
 		completion = {
